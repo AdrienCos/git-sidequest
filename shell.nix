be3cd88bdde42926cf5a95a655ff05da877f333e
Adrien Cosson <ad.cosson@gmail.com>
2024-08-08 22:12:59 +0200

    fix: cache the flake inputs in the devShell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
 {
   pkgs,
   fenix,
+  inputs,
 }:
 pkgs.mkShell {
   name = "git-sidequest";
   inputsFrom = [(pkgs.callPackage ./default.nix {fenix = fenix;})];
+  # HACK: no-op value only used to make sure the flake inputs are not GC-ed
+  # as long a profile of this flake is in the GCroots
+  keepFlakeInputs = builtins.attrValues inputs;
   packages = with pkgs; [
     bats
     just
